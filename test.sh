1993997fd8872a1ae4a9385ea2f028f11b12bf5b
Jun Zheng <arthurjordan@gmail.com>
2016-05-05 13:47:05 +1000

    Gemfile updated

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ echo "print our work directory `pwd`"
 
 source /usr/local/share/chruby/chruby.sh
 
+echo "echo 'ec2-blackout' >> Gemfile"
+echo "echo ec2-blackout added to Gemfile"
+
 RUBY=`cat .ruby-version`
 echo "Using ruby ${RUBY}"
 chruby $RUBY
