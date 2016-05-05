fe455940ac1f114a5af5a81176e97438ddc3cb41
Jun Zheng <arthurjordan@gmail.com>
2016-05-05 14:39:13 +1000

    new do_this.sh

--- do_this.sh
+++ do_this.sh
@@ -1,6 +1,20 @@
 #!/bin/bash -e
 echo "running as `whoami`"
 echo "print our work directory `pwd`"
-echo "gem info `bundle show [awesome_print]`"
-echo "bundle exec finished"
+
+source /usr/local/share/chruby/chruby.sh
+
+echo 'gem "awesome_print" ' >> Gemfile
+
+RUBY=`cat .ruby-version`
+echo "Using ruby ${RUBY}"
+chruby $RUBY
+
+echo "Before bundle install -------------------"
+bundle install
+
+echo "End bundle install -------------------"
+bundle exec ruby ./test.rb
+
+echo "bundle exec finished from inside do_this.sh"
 
