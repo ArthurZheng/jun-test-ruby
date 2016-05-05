58ed87a58d69051217503a44060b2d41a1e5502b
Jun Zheng <arthurjordan@gmail.com>
2016-05-05 14:08:10 +1000

    update do_this.sh

--- do_this.sh
+++ do_this.sh
@@ -2,18 +2,7 @@
 echo "running as `whoami`"
 echo "print our work directory `pwd`"
 
-echo "echo 'ec2-blackout' >> Gemfile"
-source /usr/local/share/chruby/chruby.sh
-
-RUBY=`cat .ruby-version`
-echo "Using ruby ${RUBY}"
-chruby $RUBY
-
-bundle install
-
-echo "bundle install run again" 
-
-bundle exec ruby ./test.rb
+echo "gem info `bundle show [ec3-blackout]`"
 
 echo "bundle exec finished"
 
